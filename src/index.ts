ca896cc186820df413e39a0c9a1abe547d383070
STEENBRINK <bram.steenbrink@hotmail.com>
2023-08-14 14:36:39 +0200

    lint-fix + app start

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import express from 'express';
 
 export default function createApp() {
   const app = express();
+
+  app.listen(parseInt(process.env.PORT, 10), () => console.info(`App up and running on port: ${process.env.PORT}`));
 }
